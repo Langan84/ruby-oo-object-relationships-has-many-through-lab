c10b1bb32cd6fd474f180fea3d9135f1fa2df545
LANGAN84 <langan.kg@gmail.com>
2021-07-19 11:14:46 +0100

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,28 @@
+class Genre
+
+    attr_accessor :name, :genre, :artist, :song
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @genre = genre
+        @artist = artist
+        @song = song
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        songs.collect {|song| song.artist}
+    end
+end
+
 
